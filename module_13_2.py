18db596324a5d1516902554580e3afb11c1f32bd
Sergey <shtem_x@mail.ru>
2025-01-31 20:18:12 +0300

    31_01_2025_20:17

--- module_13_2.py
+++ module_13_2.py
@@ -1,3 +1,21 @@
-# module_13_2
-import aiogram
+# Домашнее задание по теме "Хендлеры обработки сообщений".
 
+from aiogram import Bot,Dispatcher,types,executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from tb_api import api
+import asyncio
+
+bot = Bot(token=api)
+dp = Dispatcher(bot,storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_messsage(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
